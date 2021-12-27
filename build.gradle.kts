96aa61f587c7c1d61c8707e5e3c7a22a007097ec
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2021-12-27 17:51:29 +0100

    1.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "dev.drzepka.smarthome"
-version = "1.4.0"
+version = "1.4.1"
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 val compileKotlin: KotlinCompile by tasks
