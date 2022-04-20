106eb6af56499488e319a17059ce587a67a3dd85
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2022-04-20 19:03:41 +0200

    Update measurement model

--- src/main/kotlin/dev/drzepka/smarthome/logger/sensors/model/server/Measurement.kt
+++ src/main/kotlin/dev/drzepka/smarthome/logger/sensors/model/server/Measurement.kt
@@ -7,7 +7,7 @@ class Measurement : Serializable {
     var deviceId = 0
     var temperature: BigDecimal = BigDecimal.ZERO
     var humidity: BigDecimal = BigDecimal.ZERO
-    var batteryVoltage: BigDecimal = BigDecimal.ZERO
-    var batteryLevel = 0
+    var batteryVoltage: BigDecimal? = null
+    var batteryLevel: Int? = null
     var timestampOffsetMillis: Long? = null
 }
