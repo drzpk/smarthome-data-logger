a8619c4c4a01d71c13736677c16959faec921156
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2022-04-26 21:34:10 +0200

    Fix PV source detection

--- src/main/kotlin/dev/drzepka/smarthome/logger/pvstats/model/config/source/SourceConfigFactory.kt
+++ src/main/kotlin/dev/drzepka/smarthome/logger/pvstats/model/config/source/SourceConfigFactory.kt
@@ -6,7 +6,7 @@ import dev.drzepka.smarthome.logger.pvstats.model.config.SourceType
 object SourceConfigFactory {
 
     fun getAvailableNames(loader: ConfigurationLoader): List<String> {
-        val regex = Regex("^source\\.([a-zA-Z_-]+)\\..*$")
+        val regex = Regex("^pvstats\\.source\\.([a-zA-Z_-]+)\\..*$")
 
         val names = ArrayList<String>()
         loader.properties.keys.forEach {
